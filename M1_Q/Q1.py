4a107ee21a192427e1ce72ea3351bc787bb27518
Sannidhi-Shetty <sannishetty98@gmail.com>
2019-07-22 14:24:20 +0530

    Q1

---
+++ M1_Q/Q1.py
@@ -0,0 +1,17 @@
+#Write a progr1.	Write a program to accept a number and determine whether it is a prime number or not.
+#am to accept a number and determine whether it is a prime number or not.
+def is_prime(N):
+ if N<2:
+  return False
+ else:
+  for i in range(2,N//2+2):
+   if N % i ==0:
+     return False
+   return True
+N=int(input("Enter a number:"))
+if is_prime(N):
+ print(f"{N} is prime")
+else:
+ print(f"{N} is not prime")
+
+
